040fcb62365b5f93eb5f44a5b81f7bf5f35fff10
Tranquillia <1killer399@gmail.com>
2019-11-27 23:37:34 +0100

    Updates

---
+++ precache-manifest.d4473842d6a78f503d927ea98917728d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2d16b4b8c7c0642128cd7287fecb64",
+    "url": "/sinthgit/index.html"
+  },
+  {
+    "revision": "8c294bc11ba6d1542bce",
+    "url": "/sinthgit/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "94798c9c38030e8465b0",
+    "url": "/sinthgit/static/css/main.a5d35cd5.chunk.css"
+  },
+  {
+    "revision": "8c294bc11ba6d1542bce",
+    "url": "/sinthgit/static/js/2.f9feca08.chunk.js"
+  },
+  {
+    "revision": "94798c9c38030e8465b0",
+    "url": "/sinthgit/static/js/main.35d8d9e4.chunk.js"
+  },
+  {
+    "revision": "3e5e619993cb3b40adf5",
+    "url": "/sinthgit/static/js/runtime-main.3921fa3b.js"
+  },
+  {
+    "revision": "6967de299bc8e668733ce03e34f7aa64",
+    "url": "/sinthgit/static/media/ava-ph.6967de29.png"
+  },
+  {
+    "revision": "6c7f227f1c5f6ff077c9b3c303c40bb1",
+    "url": "/sinthgit/static/media/code.6c7f227f.svg"
+  },
+  {
+    "revision": "7a33359ed7f526c499664724b93826ef",
+    "url": "/sinthgit/static/media/dreamer.7a33359e.svg"
+  },
+  {
+    "revision": "c93eba2f914725edbbafc7ae0af63535",
+    "url": "/sinthgit/static/media/globe.c93eba2f.svg"
+  },
+  {
+    "revision": "ede92533884e7c8744a177df592ceab8",
+    "url": "/sinthgit/static/media/welcome.ede92533.svg"
+  }
+]);
